f614043915a44155e43f0336fc21cc9a4074996b
juanlam7 <juanlam7@gmail.com>
2024-12-09 14:33:55 +0100

    feat: Change up and down icons to scroll

--- src/app/components/up-down/up-down.component.ts
+++ src/app/components/up-down/up-down.component.ts
@@ -3,16 +3,22 @@ import { Component, input } from '@angular/core';
 @Component({
   selector: 'app-up-down',
   template: `
-    <div class="flex justify-center space-x-4 min-h-10 my-2">
+    <div class="flex justify-center space-x-4 min-h-10 my-1">
       <button
         (click)="
           showBottomButton()
             ? scrollToBottom(scrollableDiv())
             : scrollToTop(scrollableDiv())
         "
-        class="bg-slate-700 text-white px-4 py-2 mb-1 rounded text-xs"
+        class="bg-slate-700 text-white p-2 rounded text-xs"
       >
-        {{ showBottomButton() ? 'Bottom' : 'Top' }}
+        <div class="flex items-center justify-center">
+          <img
+            [src]="showBottomButton() ? '/arrow-down.svg' : '/arrow-up.svg'"
+            class="w-6 h-6"
+            alt="Custom Icon"
+          />
+        </div>
       </button>
     </div>
   `,
@@ -20,7 +26,7 @@ import { Component, input } from '@angular/core';
 export class UpDownComponent {
   showBottomButton = input.required<boolean>();
   scrollableDiv = input.required<HTMLDivElement>();
-  
+
   scrollToTop(element: HTMLElement) {
     element.scrollTo({ top: 0, behavior: 'smooth' });
   }
